86bb4f6a65faf103a8a6a7974b01799c7b2c7a04
Joshua Huang <jah370@case.edu>
2022-12-28 01:21:31 -0600

    tutorial cont.

--- mujoco_bringup.py
+++ mujoco_bringup.py
@@ -1,7 +1,7 @@
 #%%
 #import mujoco
 import mujoco
-#%% Import Packages for Plotting and Creating Graphics
+#Import Packages for Plotting and Creating Graphics
 import time
 import itertools
 import numpy as np
@@ -17,6 +17,9 @@ import matplotlib.pyplot as plt
 # More legible printing from numpy.
 np.set_printoptions(precision=3, suppress=True, linewidth=100)
 
+# ******************RUN BELOW RENDERS INDIVIDUALLY*************
+
+# *************************************************************
 #%% Load A Simple Model
 xml = """
 <mujoco>
@@ -41,4 +44,301 @@ mujoco.mj_forward(model, data)
 renderer.update_scene(data)
 media.show_image(renderer.render())
 
-# %%
+#%% Creating a render with a light
+xml = """
+<mujoco>
+  <worldbody>
+    <light name="top" pos="0 0 1"/>
+    <geom name="red_box" type="box" size=".2 .2 .2" rgba="1 0 0 1"/>
+    <geom name="green_sphere" pos=".2 .2 .2" size=".1" rgba="0 1 0 1"/>
+  </worldbody>
+</mujoco>
+"""
+model_light = mujoco.MjModel.from_xml_string(xml)
+data = mujoco.MjData(model_light)
+renderer = mujoco.Renderer(model_light)
+
+mujoco.mj_forward(model_light, data)
+renderer.update_scene(data)
+
+media.show_image(renderer.render())
+
+
+# %% Render different color box
+# Run this cell multiple times for different colors
+model_light.geom('red_box').rgba[:3] = np.random.rand(3)
+renderer.update_scene(data)
+media.show_image(renderer.render())
+
+# %% Simulation
+duration = 3.8 # seconds
+framerate = 60 # hz
+
+#simulate and display video
+frames = []
+mujoco.mj_resetData(model_light, data) #reset state and time
+while data.time < duration:
+  mujoco.mj_step(model_light, data)
+  if len(frames) < data.time * framerate:
+    renderer.update_scene(data)
+    pixels = renderer.render().copy()
+    frames.append(pixels)
+  
+media.show_video(frames, fps=framerate)
+
+#%% Adding DOFs for Movement
+xml = """
+<mujoco>
+  <worldbody>
+    <light name="top" pos="0 0 1"/>
+    <body name="box_and_sphere" euler="0 0 -30">
+      <joint name="swing" type="hinge" axis="1 -1 0" pos="-.2 -.2 -.2"/>
+      <geom name="red_box" type="box" size=".2 .2 .2" rgba="1 0 0 1"/>
+      <geom name="green_sphere" pos=".2 .2 .2" size=".1" rgba="0 1 0 1"/>
+    </body>
+  </worldbody>
+</mujoco>
+"""
+model = mujoco.MjModel.from_xml_string(xml)
+data = mujoco.MjData(model)
+renderer = mujoco.Renderer(model)
+
+# enable joint visualization option:
+scene_option = mujoco.MjvOption()
+scene_option.flags[mujoco.mjtVisFlag.mjVIS_JOINT] = True
+
+duration = 3.8  # (seconds)
+framerate = 60  # (Hz)
+
+frames = []
+mujoco.mj_resetData(model, data)
+while data.time < duration:
+  mujoco.mj_step(model, data)
+  if len(frames) < data.time * framerate:
+    renderer.update_scene(data, scene_option=scene_option)
+    pixels = renderer.render().copy()
+    frames.append(pixels)
+
+# Simulate and display video.
+media.show_video(frames, fps=framerate)
+
+# %%#Flipping gravity and rendering again
+print('default gravity', model.opt.gravity)
+model.opt.gravity = (0, 0, 10)
+print('flipped gravity', model.opt.gravity)
+frames = []
+mujoco.mj_resetData(model, data)
+while data.time < duration:
+  mujoco.mj_step(model, data)
+  if len(frames) < data.time * framerate:
+    renderer.update_scene(data, scene_option = scene_option)
+    pixels = renderer.render().copy()
+    frames.append(pixels)
+
+media.show_video(frames, fps=60)
+
+# %% Example: simulating free bodies with the self inverting "tippe-top"
+tippe_top = """
+<mujoco model="tippe top">
+  <option integrator="RK4"/>
+
+  <asset>
+    <texture name="grid" type="2d" builtin="checker" rgb1=".1 .2 .3"
+     rgb2=".2 .3 .4" width="300" height="300"/>
+    <material name="grid" texture="grid" texrepeat="8 8" reflectance=".2"/>
+  </asset>
+
+  <worldbody>
+    <geom size=".2 .2 .01" type="plane" material="grid"/>
+    <light pos="0 0 .6"/>
+    <camera name="closeup" pos="0 -.1 .07" xyaxes="1 0 0 0 1 2"/>
+    <body name="top" pos="0 0 .02">
+      <freejoint/>
+      <geom name="ball" type="sphere" size=".02" />
+      <geom name="stem" type="cylinder" pos="0 0 .02" size="0.004 .008"/>
+      <geom name="ballast" type="box" size=".023 .023 0.005"  pos="0 0 -.015"
+       contype="0" conaffinity="0" group="3"/>
+    </body>
+  </worldbody>
+
+  <keyframe>
+    <key name="spinning" qpos="0 0 0.02 1 0 0 0" qvel="0 0 0 0 1 200" />
+  </keyframe>
+</mujoco>
+"""
+model = mujoco.MjModel.from_xml_string(tippe_top)
+renderer = mujoco.Renderer(model)
+data = mujoco.MjData(model)
+mujoco.mj_forward(model, data)
+renderer.update_scene(data, camera="closeup")
+media.show_image(renderer.render())
+
+# %% #simulate and display top render
+
+duration = 7    # (seconds)
+framerate = 60  # (Hz)
+
+# Simulate and display video.
+frames = []
+mujoco.mj_resetDataKeyframe(model, data, 0)  # Reset the state to keyframe 0
+while data.time < duration:
+  mujoco.mj_step(model, data)
+  if len(frames) < data.time * framerate:
+    renderer.update_scene(data, "closeup")
+    pixels = renderer.render().copy()
+    frames.append(pixels)
+
+media.show_video(frames, fps=framerate)
+# %% Measuring Data from mjData
+timevals = []
+angular_velocity = []
+stem_height = []
+
+#simulate and save data
+mujoco.mj_resetDataKeyframe(model, data, 0)
+while data.time < duration:
+  mujoco.mj_step(model, data)
+  timevals.append(data.time)
+  angular_velocity.append(data.qvel[3:6].copy())
+  stem_height.append(data.geom_xpos[2,2]);
+
+dpi = 120
+width = 600
+height = 800
+figsize = (width / dpi, height/dpi)
+_, ax = plt.subplots(2, 1, figsize=figsize, dpi=dpi, sharex=True)
+
+ax[0].plot(timevals, angular_velocity)
+ax[0].set_title('angular velocity')
+ax[0].set_ylabel('radians / second')
+
+ax[1].plot(timevals, stem_height)
+ax[1].set_xlabel('time (seconds)')
+ax[1].set_ylabel('meters')
+_ = ax[1].set_title('stem height')
+# %% Example: A chaotic pendulum
+chaotic_pendulum = """
+<mujoco>
+  <option timestep=".001">
+    <flag energy="enable" contact="disable"/>
+  </option>
+
+  <default>
+    <joint type="hinge" axis="0 -1 0"/>
+    <geom type="capsule" size=".02"/>
+  </default>
+
+  <worldbody>
+    <light pos="0 -.4 1"/>
+    <camera name="fixed" pos="0 -1 0" xyaxes="1 0 0 0 0 1"/>
+    <body name="0" pos="0 0 .2">
+      <joint name="root"/>
+      <geom fromto="-.2 0 0 .2 0 0" rgba="1 1 0 1"/>
+      <geom fromto="0 0 0 0 0 -.25" rgba="1 1 0 1"/>
+      <body name="1" pos="-.2 0 0">
+        <joint/>
+        <geom fromto="0 0 0 0 0 -.2" rgba="1 0 0 1"/>
+      </body>
+      <body name="2" pos=".2 0 0">
+        <joint/>
+        <geom fromto="0 0 0 0 0 -.2" rgba="0 1 0 1"/>
+      </body>
+      <body name="3" pos="0 0 -.25">
+        <joint/>
+        <geom fromto="0 0 0 0 0 -.2" rgba="0 0 1 1"/>
+      </body>
+    </body>
+  </worldbody>
+</mujoco>
+"""
+
+model = mujoco.MjModel.from_xml_string(chaotic_pendulum)
+renderer = mujoco.Renderer(model, 480, 640)
+data = mujoco.MjData(model)
+mujoco.mj_forward(model, data)
+renderer.update_scene(data, camera="fixed")
+media.show_image(renderer.render())
+
+# %% Timing The Chaotic Pendulum
+#Xml file
+chaotic_pendulum = """
+<mujoco>
+  <option timestep=".001">
+    <flag energy="enable" contact="disable"/>
+  </option>
+
+  <default>
+    <joint type="hinge" axis="0 -1 0"/>
+    <geom type="capsule" size=".02"/>
+  </default>
+
+  <worldbody>
+    <light pos="0 -.4 1"/>
+    <camera name="fixed" pos="0 -1 0" xyaxes="1 0 0 0 0 1"/>
+    <body name="0" pos="0 0 .2">
+      <joint name="root"/>
+      <geom fromto="-.2 0 0 .2 0 0" rgba="1 1 0 1"/>
+      <geom fromto="0 0 0 0 0 -.25" rgba="1 1 0 1"/>
+      <body name="1" pos="-.2 0 0">
+        <joint/>
+        <geom fromto="0 0 0 0 0 -.2" rgba="1 0 0 1"/>
+      </body>
+      <body name="2" pos=".2 0 0">
+        <joint/>
+        <geom fromto="0 0 0 0 0 -.2" rgba="0 1 0 1"/>
+      </body>
+      <body name="3" pos="0 0 -.25">
+        <joint/>
+        <geom fromto="0 0 0 0 0 -.2" rgba="0 0 1 1"/>
+      </body>
+    </body>
+  </worldbody>
+</mujoco>
+"""
+
+model = mujoco.MjModel.from_xml_string(chaotic_pendulum)
+data = mujoco.MjData(model)
+# setup
+n_seconds = 6
+framerate = 30  # Hz
+n_frames = int(n_seconds * framerate)
+frames = []
+renderer = mujoco.Renderer(model, 240, 320)
+
+
+# set initial state
+mujoco.mj_resetData(model, data)
+data.joint('root').qvel = 10
+
+
+# simulate and record frames
+frame = 0
+sim_time = 0
+render_time = 0
+n_steps = 0
+for i in range(n_frames):
+  while data.time * framerate < i:
+    tic = time.time()
+    mujoco.mj_step(model, data)
+    sim_time += time.time() - tic
+    n_steps += 1
+  tic = time.time()
+  renderer.update_scene(data, "fixed")
+  frame = renderer.render()
+  render_time += time.time() - tic
+  frames.append(frame.copy())
+
+# print timing and play video
+step_time = 1e6*sim_time/n_steps
+step_fps = n_steps/sim_time
+print(f'simulation: {step_time:5.3g} μs/step  ({step_fps:5.0f}Hz)')
+frame_time = 1e6*render_time/n_frames
+frame_fps = n_frames/render_time
+print(f'rendering:  {frame_time:5.3g} μs/frame ({frame_fps:5.0f}Hz)')
+print('\n')
+
+# show video
+media.show_video(frames, fps=framerate)
+# %% Chaotic Pendulum
+#Small pertubations in initial conditions accumulate quickly
+
